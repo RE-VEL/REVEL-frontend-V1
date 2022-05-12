8a010af9635236db9df945063e2fb701670a99d5
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-05-12 18:07:22 +0900

    Feat: 화면에 header 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import '../styles/font.css';
 import type { AppProps } from 'next/app';
+import Header from '../components/common/header/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />;
+    </div>
+  );
 }
 
 export default MyApp;
