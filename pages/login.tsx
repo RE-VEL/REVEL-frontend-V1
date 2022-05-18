e1febaa16bd2f8a4bb82510e98595727ea231323
onraon <onepunch2019@naver.com>
2022-05-18 19:11:07 +0900

    Style: add HTML

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,14 @@
 import { NextPage } from 'next';
 
-
-const Login:NextPage()=>{
-return 
-    <Page>
-        
-    </Page>
-}
-
-
+const Login: NextPage = () => {
+    return <div id="id01" className='modal'>
+        <form className='modal-content animate'>
+            <input type="text" placeholder='아이디를 입력해 주세요.' name='uname' aria-required/>
+            <input type="text" placeholder='비밀번호를 입력해 주세요.' name='psw' aria-required/>
+            <button type="submit">로그인</button>
+        </form>
+        <label htmlFor="join"><b>회원가입</b></label>
+    </div>
+};
 
 export default Login;
