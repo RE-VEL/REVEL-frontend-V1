8d13ddcd1943a3cc589ead61a4b063aeb10f3045
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-05-19 22:33:40 +0900

    Feat: my club list

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,5 +1,62 @@
 import styled from '@emotion/styled';
 import { NextPage } from 'next';
+import Slider from 'react-slick';
+import { sliderSetting } from '../interface/sliderSetting';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { card } from '../interface/card';
+
+const settings: sliderSetting = {
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+};
+
+const cards = [
+  {
+    id: 1,
+    clubName: 'Reverie',
+    tags: [
+      '전공동아리운영위원회전공동아리운영위원회전공동아리운영위원회전공동아리운영위원회',
+      '자율동아리',
+    ],
+    banner: '/img/banner.png',
+    icon: '/img/icon.png',
+  },
+  {
+    id: 2,
+    clubName: 'Reverie',
+    tags: ['전공동아리운영위원회', '자율동아리'],
+    banner: '/img/banner.png',
+    icon: '/img/icon.png',
+  },
+  {
+    id: 2,
+    clubName: 'Reverie',
+    tags: ['전공동아리운영위원회', '자율동아리'],
+    banner: '/img/banner.png',
+    icon: '/img/icon.png',
+  },
+  {
+    id: 2,
+    clubName: 'Reverie',
+    tags: ['전공동아리운영위원회', '자율동아리'],
+    banner: '/img/banner.png',
+    icon: '/img/icon.png',
+  },
+  {
+    id: 2,
+    clubName: 'Reverie',
+    tags: ['전공동아리운영위원회', '자율동아리'],
+    banner: '/img/banner.png',
+    icon: '/img/icon.png',
+  },
+];
 
 const MyProfile: NextPage = () => {
   return (
@@ -13,10 +70,28 @@ const MyProfile: NextPage = () => {
         </MyInfo>
         <CreateClubBtn>동아리 생성</CreateClubBtn>
       </InfoWraper>
-      <div></div>
+      <div>
+        <SliderSection {...settings}>
+          <MyClub>3</MyClub>
+          <MyClub>3</MyClub>
+          <MyClub>3</MyClub>
+          <MyClub>3</MyClub>
+        </SliderSection>
+      </div>
     </MyPage>
   );
 };
+
+const SliderSection = styled(Slider)`
+  width: 100%;
+  height: 100%;
+  background-color: red;
+`;
+
+const MyClub = styled.div`
+  background-color: blue;
+  margin: 10px;
+`;
 
 const MyPage = styled.main`
   width: 100vw;
