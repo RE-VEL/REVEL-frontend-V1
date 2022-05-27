5d293d5529337af84cf87723c6412be200c03148
zn0228 <zn0228@naver.com>
2022-05-27 20:17:11 +0900

    Style: 배너이미지 삽입

--- pages/reverie.tsx
+++ pages/reverie.tsx
@@ -1,7 +1,15 @@
 import { NextPage } from 'next';
+import styled from 'styled-components';
 
 const Reverie: NextPage = () => {
-  return <div>개발자페이지</div>;
+  return <ImgDiv></ImgDiv>;
 };
+
+const ImgDiv = styled.div`
+  height: 800px;
+  width: 250px;
+  background-image: url('/img/revel_devpage.png');
+  background-color: blue;
+`;
 
 export default Reverie;
