887d6eff7555d8f2148a89fa1db7e570eaba9b92
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-06-06 21:29:03 +0900

    Style: AboutClub section fadeIn과 fadeOut animation이 동작하는 스크롤 위치 분리

--- components/main/aboutClub/backCircle/BackCircle.tsx
+++ components/main/aboutClub/backCircle/BackCircle.tsx
@@ -6,12 +6,23 @@ import CircleWrap from './CircleWrap';
 
 const BackCircle: NextPage = () => {
   const [isShow, setIsShow] = useState<boolean>(false);
+  const [isEnter, setIsEnter] = useState<boolean>(false);
 
   const handleScroll = () => {
-    setIsShow(
+    if (
+      !isEnter &&
       window.innerHeight * 0.2 < window.scrollY &&
-        window.innerHeight * 1.8 > window.scrollY,
-    );
+      window.innerHeight * 1.8 > window.scrollY
+    ) {
+      setIsShow(true);
+      setIsEnter(true);
+    } else if (
+      0 === window.scrollY ||
+      window.innerHeight * 2 <= window.scrollY
+    ) {
+      setIsShow(false);
+      setIsEnter(false);
+    }
   };
 
   useEffect(() => {
