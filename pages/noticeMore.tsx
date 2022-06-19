0fd549ac077499f04ca7a2370683fa0974a04c76
zn0228 <zn0228@naver.com>
2022-06-19 18:36:33 +0900

    Style:글자삽입

---
+++ pages/noticeMore.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+import { NextPage } from 'next';
+
+const NoticeMore: NextPage = () => {
+  return (
+    <div>
+      <p>Notice</p>
+    </div>
+  );
+};
+
+const Notice = styled.p`
+  font-size: 50px;
+`;
+
+export default NoticeMore;
