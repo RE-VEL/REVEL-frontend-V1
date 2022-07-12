0ed6f26ad95d02e602e981baf2275b10981b97df
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-07-12 22:32:14 +0900

    Feat: 400 error시 경고 출력

--- src/utils/apis/login.ts
+++ src/utils/apis/login.ts
@@ -1,16 +1,20 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 import { LoginDataType } from 'src/interface/login';
 import { saveToken } from '../function/tokenManager';
 
 export const login = async (loginData: LoginDataType) => {
-  const response: AxiosResponse = await axios.post(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/user/login`,
-    { ...loginData },
-  );
+  try {
+    const response: AxiosResponse = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/user/login`,
+      { ...loginData },
+    );
 
-  console.log(response);
-  const { accessToken, refreshToken } = response.data;
-  saveToken(accessToken, refreshToken);
+    console.log(response);
+    const { accessToken, refreshToken } = response.data;
+    saveToken(accessToken, refreshToken);
 
-  return response.status;
+    return response.status;
+  } catch (err: any) {
+    return err.response.status;
+  }
 };
